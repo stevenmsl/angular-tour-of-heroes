5f782d271ad3194f5ed63fe820a566d386c3bc67
Steven Lin <slin@mac.local>
2019-08-07 14:13:46 -0700

    Add event binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 //Specifies the Angular metadata for the component 
 @Component({
@@ -9,13 +10,20 @@ import { Hero } from '../hero';
   */
   selector: 'app-heroes', 
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'] //private style to override global styles 
 })
 export class HeroesComponent implements OnInit {
   hero : Hero = {
     id : 1,
     name : 'Windstorm'
   };
+
+  heroes = HEROES; //Expose the HEROES array for binding 
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
