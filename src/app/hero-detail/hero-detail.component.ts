18f2535a14133b73c18eaf4c694b19386175f6a5
Steven Lin <slin@mac.local>
2019-08-09 15:51:32 -0700

    Complete Master/Detail Components

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  //external component will bind to it
+  @Input() hero: Hero; 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
